9419be78b0084b99617043fe24163810372c2642
Chris Kelner <chris.kelner@gmail.com>
2013-03-08 21:01:36 -0500

    can add some shit to the localstorage

--- resources/js/controllers.js
+++ resources/js/controllers.js
@@ -1,9 +1,14 @@
 function ReservationController($scope) {
- 	$scope.reservations = [];
+	$scope.reservations = [];
  	$scope.customer = "";
  	$scope.howMany = "";
     $scope.phone = "";
 	$scope.addReservation = function() {
+		if(localStorage.reservations!="") {
+			$scope.reservations = JSON.parse(localStorage.reservations);
+		} else {
+			$scope.reservations = [];
+		}
 		$scope.reservations.push({
             /*customer: $scope.customer,
             howMany: $scope.howMany,
@@ -12,6 +17,7 @@ function ReservationController($scope) {
             howMany: "12",
             phone: "12345678"
         });
+        localStorage.reservations = JSON.stringify($scope.reservations);
         $scope.customer="test";
         $scope.howMany= "12";
         $scope.phone= "12345678";
